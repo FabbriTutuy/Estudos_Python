5bd724da2fc34e0f0b6d8ab40ce54aad9b990ae1
Arthur Fabbri <arthurfabbri2406@gmail.com>
2026-05-16 18:37:20 -0300

    Correção de exercicios de POO

--- Python Curso em vídeo/POO/desafios/desafio16.py
+++ Python Curso em vídeo/POO/desafios/desafio16.py
@@ -1,18 +1,36 @@
 from rich import print
+from rich import inspect
 
 class Funcionario():
     
-    def __init__(self,nome,setor,cargo,empresa="Curso em Video"):
+    empresa = "Curso em Vídeo"
+
+    def __init__(self,nome,setor,cargo):
         
         self.nome = nome
         self.setor = setor
         self.cargo = cargo
-        self.empresa = empresa
 
-    def Apresentar(self):
+    def Apresentar(self) -> str:
 
-        return f"Olá, sou [bold blue]{self.nome}[/] e sou {self.cargo} no setor de {self.setor} da empresa {self.empresa}"
+        return f":handshake: Olá, sou [bold blue]{self.nome}[/] e sou {self.cargo} no setor de {self.setor} da empresa {Funcionario.empresa}."
 
 
 c1 = Funcionario("Pedro","TI" , "PROGRAMADOR JAVA")
+c2 = Funcionario("Maria","Administração","Financeiro")
 print(c1.Apresentar())
+print(c2.Apresentar())
+
+
+#inspect(c1)    ----> SE UTILIZAR O INSPECT APARECERÁ ASSIM NO TERMINAL
+
+#╭──────────── <class '__main__.Funcionario'> ─────────────╮
+#│ ╭─────────────────────────────────────────────────────╮ │
+#│ │ <__main__.Funcionario object at 0x00000272B6018C20> │ │
+#│ ╰─────────────────────────────────────────────────────╯ │
+#│                                                         │
+#│   cargo = 'PROGRAMADOR JAVA'                            │
+#│ empresa = 'Curso em Video'                              │
+#│    nome = 'Pedro'                                       │
+#│   setor = 'TI'                                          │
+#╰─────────────────────────────────────────────────────────╯
--- Python Curso em vídeo/POO/desafios/desafio17.py
+++ Python Curso em vídeo/POO/desafios/desafio17.py
@@ -4,16 +4,18 @@ from rich.panel import Panel
 class Produto():
     
     def __init__(self,nome,preco):
-
         self.nome = nome
         self.preco = preco
 
     
     def etiqueta(self):
         
-        x = Panel(f"[bold white]{self.nome:.^35}\nR${self.preco:.^33}[/]",title="Produto")
-        print(x)
+        conteudo = f"{self.nome.center(30, " ")}"
+        conteudo += f"{'-'*30}"
+        precof = f" R${self.preco:,.2f} "
+        conteudo += f"{precof.center(30,'-')}"
+        etiqueta_preco = Panel(conteudo,title="Produto",width=34)
+        print(etiqueta_preco)
 
-
-p1 = Produto("Iphone 17 Pro max",12000.43)
+p1 = Produto("Iphone 17 Pro max",12_000.85)
 p1.etiqueta()
